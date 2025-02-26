c7981ed500eeb27a8fc687414dbea20c4d1a0537
Arghya Sarkar <63844005+Arghya-1121@users.noreply.github.com>
2025-02-26 23:38:10 +0530

    Sample apis.dart file.

--- lib/Fetch/fetch_data.dart
+++ lib/Fetch/fetch_data.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'package:weather03/api.dart';
+import 'package:weather03/apis.dart';
 
 class Weather {
   final String city;
