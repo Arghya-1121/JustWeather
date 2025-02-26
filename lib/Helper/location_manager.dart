caf29f9429707583ce7788c032fdd3e49ee846e9
Arghya Sarkar <63844005+Arghya-1121@users.noreply.github.com>
2025-02-27 01:07:49 +0530

    Add location manager to save locations using shared preferences.

---
+++ lib/Helper/location_manager.dart
@@ -0,0 +1,30 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LocationManager {
+  static const String _key = 'Location';
+
+  static Future<void> saveLocations(List<String> loc) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList(_key, loc);
+  }
+
+  static Future<List<String>> getLocation() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getStringList(_key) ?? [];
+  }
+
+  static Future<void> addLocation(String loc) async {
+    final locs = await getLocation();
+    if (locs.contains(loc)) {
+      throw Exception('Location already exists');
+    }
+    locs.add(loc);
+    await saveLocations(locs);
+  }
+
+  static Future<void> removeLocation(String loc) async {
+    final locs = await getLocation();
+    locs.remove(loc);
+    await saveLocations(locs);
+  }
+}
