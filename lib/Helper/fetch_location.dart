67a48b8ae9bd8f4634aac42e397f73540f2a6e2a
Arghya Sarkar <63844005+Arghya-1121@users.noreply.github.com>
2025-03-04 13:24:38 +0530

    can be ignored

---
+++ lib/Helper/fetch_location.dart
@@ -0,0 +1,34 @@
+// import 'package:geolocator/geolocator.dart';
+// import 'package:geocoding/geocoding.dart';
+
+// Future<String> currentLocation() async {
+//   LocationPermission locationPermission = await Geolocator.checkPermission();
+//   if (locationPermission == LocationPermission.denied){
+//     locationPermission = await Geolocator.requestPermission();
+//   }
+//   Position currentPosition = await Geolocator.getCurrentPosition();
+//   List<Placemark> currentLoc = await placemarkFromCoordinates(currentPosition.latitude, currentPosition.longitude);
+//   String? nearby = currentLoc[0].locality;
+//   // print("\n\n\n${nearby}");
+//   return nearby??"";
+// }
+
+import 'package:geolocator/geolocator.dart';
+
+Future<Position> getCurrentLocation() async {
+  bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    throw 'Location services are disabled.';
+  }
+  LocationPermission permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      throw 'Location permissions are denied';
+    }
+  }
+  if (permission == LocationPermission.deniedForever) {
+    throw 'Location permissions are permanently denied, we cannot request permissions.';
+  }
+  return await Geolocator.getCurrentPosition();
+}
