d137ae40d793d78646fe53dd2ab9b72b41935975
Arghya Sarkar <63844005+Arghya-1121@users.noreply.github.com>
2025-03-12 01:25:41 +0530

    Refactor: Rename Android and iOS app package and display names
    - Change the Android package name from `com.example.weather03` to `com.arghya.justweather`.
    - Change the iOS app display name and bundle identifier to `Just Weather` and `com.arghya.justweather`, respectively.
    - Update the Android app's label in `AndroidManifest.xml` to `Just Weather`.
    - Remove the MainActivity.kt
    - Added `change_app_package_name` to `pubspec.yaml`.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.weather03"
+    namespace = "com.arghya.justweather"
     compileSdk = flutter.compileSdkVersion
 //    ndkVersion = flutter.ndkVersion
     ndkVersion = "27.0.12077973"
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.weather03"
+        applicationId = "com.arghya.justweather"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
